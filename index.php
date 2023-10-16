adc7fb151311fea611251654a111bbd40058a316
unknown <ashadipermana2@gmail.com>
2023-10-17 07:48:07 +0800

    Carousel dan Footer

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ if (isset($_SESSION['username'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="./style/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://kit.fontawesome.com/1a7210c043.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -69,10 +70,10 @@ if (isset($_SESSION['username'])) {
         <section>
             <div class="isi-1 bg" id="isi-1">
                 <div>
-                    <h1>Paimon</h1>
+                    <h1>Selamat Datang di Surga Rasa Eskrim</h1>
                     <p>
-                        Paimon merupakan pemandu pemain Genshin Impact dari awal permainan hingga menjadi kawan seperjalanan dan senantiasa menemani Traveler berpetualang di dunia Teyvat.
-                    </p>
+                    Dari Es Krim Klasik yang Memikat Hati Hingga Inovasi Rasa Terkini yang Menggetarkan Lidah. Cicipi Sensasi yang Luar Biasa, Temukan Setiap Gigitan Penuh Kebahagiaan.
+                   </p>
                     <button class="btn btn-outline-danger mt-3">Lihat Menu Selengkapnya...</button>
                 </div>
                 <img src="./assets/eskrim.png" alt="paimon" style="max-width: 40vw; height: auto; ">
@@ -81,50 +82,52 @@ if (isset($_SESSION['username'])) {
         <!-- end landing page -->
 
         <!-- catalog -->
-        <section class="p-4" id="katalog">
+        <section class="pr-0 pl-0 pt-0 pb-5 mt-0" id="katalog">
+            <!-- CAROSEL -->
+            <div class="container1">
+                <div id="carouselExampleDark" class="carousel carousel-light slide pb-0" data-bs-ride="carousel">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active" data-bs-interval="10000">
+                            <img src="https://mockuptree.com/wp-content/uploads/edd/2022/07/Free-ice-cream-mockup-960x640.jpg" class="d-block w-100 " alt="...">
+                            <div class="tes carousel-caption d-none d-md-block">
+                                <h5>Favorite Menu #1</h5>
+                                <p>Twisted Ice Cream.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item" data-bs-interval="2000">
+                            <img src="https://img.freepik.com/premium-psd/3-oz-paper-ice-cream-cups-mockup_1332-1517.jpg" class="d-block w-100" alt="...">
+                            <div class="tes carousel-caption d-none d-md-block">
+                                <h5>Favorite Menu #2</h5>
+                                <p>Banana Ice Cream.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://houseofmockups.com/cdn/shop/products/HOM-000-007D-Ice-Cream-Cone-Wrapper_860x.jpg?v=1606780495" class="d-block w-100" alt="...">
+                            <div class="tes carousel-caption d-none d-md-block">
+                                <h5>Favorite Menu #3</h5>
+                                <p>Vanilla Crunchy Ice Cream Cone.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+                    <!-- END CAROSEL -->
             <div class="katalog container-fluid p-4">
                 <div class="container">
                     <h1 class="p-5 pb-4" style="text-align: center; font-weight:800;">Catalog</h1>
-                    <!-- CAROSEL -->
-                    <div id="carouselExampleDark" class="carousel carousel-light slide col-lg-8 offset-lg-2 pb-5" data-bs-ride="carousel">
-                        <div class="carousel-indicators">
-                            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                        </div>
-                        <div class="carousel-inner">
-                            <div class="carousel-item active" data-bs-interval="10000">
-                                <img src="https://mockuptree.com/wp-content/uploads/edd/2022/07/Free-ice-cream-mockup-960x640.jpg" class="d-block w-100 " alt="...">
-                                <div class="tes carousel-caption d-none d-md-block">
-                                    <h5>Favorite Menu #1</h5>
-                                    <p>Twisted Ice Cream.</p>
-                                </div>
-                            </div>
-                            <div class="carousel-item" data-bs-interval="2000">
-                                <img src="https://img.freepik.com/premium-psd/3-oz-paper-ice-cream-cups-mockup_1332-1517.jpg" class="d-block w-100" alt="...">
-                                <div class="tes carousel-caption d-none d-md-block">
-                                    <h5>Favorite Menu #2</h5>
-                                    <p>Banana Ice Cream.</p>
-                                </div>
-                            </div>
-                            <div class="carousel-item">
-                                <img src="https://houseofmockups.com/cdn/shop/products/HOM-000-007D-Ice-Cream-Cone-Wrapper_860x.jpg?v=1606780495" class="d-block w-100" alt="...">
-                                <div class="tes carousel-caption d-none d-md-block">
-                                    <h5>Favorite Menu #3</h5>
-                                    <p>AVanilla Crunchy Ice Cream Cone.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Previous</span>
-                        </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Next</span>
-                        </button>
-                    </div>
-                    <!-- END CAROSEL -->
                     <div class="row justify-content-center">
                         <div class="col-md-4">
                             <div class="card bg-light">
@@ -239,10 +242,28 @@ if (isset($_SESSION['username'])) {
 
 
     </main>
-
-    <footer>
-        <p>Copyright © Liyue Ice Cream. All Rights Reserved.</p>
-    </footer>
+    
+    <footer class="footer">
+    <div class="container py-4">
+        <div class="row">
+            <div class="col-md-6">
+                <h5 class="mb-4">Kontak Kami</h5>
+                <p><i class="fas fa-map-marker-alt"></i> Alamat, Kota, Negara</p>
+                <p><i class="fas fa-phone"></i> +123456789</p>
+                <p><i class="fas fa-envelope"></i> Liyue_ice_cream@gmail.com</p>
+            </div>
+            <div class="col-md-6">
+                <h5 class="icon mb-4">Ikuti Kami</h5>
+                <a href="#" class="text-light me-2"><i class="fab fa-facebook"></i></a>
+                <a href="#" class="text-light me-2"><i class="fab fa-twitter"></i></a>
+                <a href="#" class="text-light me-2"><i class="fab fa-instagram"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class=" footer-end text-center p-3" style="background-color: var(--text2);">
+    <p>Copyright © Liyue Ice Cream. All Rights Reserved.</p>
+    </div>
+</footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="./script/main.js"></script>
